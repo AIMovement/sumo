60104c0fae70ad6f641f84266855fa9b78cf52fc
Jonas <osannolik@godtycklig.se>
2018-12-19 21:12:50 +0100

    Remove pyglet crap from server-side

--- rpi/rc_server.py
+++ rpi/rc_server.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 from robot_communication import Robot_IO
-import pyglet
-from pyglet.window import key
 import time
 
 import grpc
@@ -13,66 +11,6 @@ from collections import deque
 from concurrent import futures
 import threading
 import argparse
-
-class KeyboardListener(object):
-
-    def __init__(self, cmdqueue):
-        super(KeyboardListener, self).__init__()
-
-        self.cmdqueue = cmdqueue
-
-        self.keyboard = pyglet.window.key.KeyStateHandler()
-        self.win = pyglet.window.Window()
-        self.win.push_handlers(self.keyboard)
-
-        self._cmd = (0.0, 0.0)
-        self.cmdqueue.append(self._cmd)
-
-        pyglet.clock.schedule_interval(self.command_update, 0.05)
-
-    def is_pressed(self, k):
-        return self.keyboard[k]
-
-    def command_update(self, dt):
-        if self.is_pressed(key.Q):
-            quit()
-
-        elif self.is_pressed(key.UP):
-            if self.is_pressed(key.LEFT):
-                cmd = (0.5, 1.0)
-            elif self.is_pressed(key.RIGHT):
-                cmd = (1.0, 0.5)
-            elif self.is_pressed(key.DOWN):
-                cmd = (0.0, 0.0)
-            else:
-                cmd = (1.0, 1.0)
-
-        elif self.is_pressed(key.LEFT):
-            if self.is_pressed(key.DOWN):
-                cmd = (-0.5, -1.0)
-            elif self.is_pressed(key.RIGHT):
-                cmd = (0.0, 0.0)
-            else:
-                cmd = (-0.5, 0.5)
-
-        elif self.is_pressed(key.RIGHT):
-            if self.is_pressed(key.DOWN):
-                cmd = (-1.0, -0.5)
-            else:
-                cmd = (0.5, -0.5)
-
-        elif self.is_pressed(key.DOWN):
-            cmd = (-1.0, -1.0)
-
-        else:
-            cmd = (0.0, 0.0)
-
-        scale = 0.5
-        cmd = (scale*cmd[0], scale*cmd[1])
-
-        if cmd != self._cmd:
-            self.cmdqueue.append(cmd)
-            self._cmd = cmd
 
 class Remote_IO(sumo_pb2_grpc.SumoProtocolServicer):
     ''' Receive commands and send sensor data via grpc.
@@ -157,7 +95,7 @@ def SerialTasks(rio, cmdqueue, sensorqueue):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--ip_port', help='IP-port used for remote mode')
+    parser.add_argument('-p', '--ip_port', default='50051', help='IP-port used for remote mode')
     parser.add_argument('-s', '--serial_port', default='/dev/ttyUSB0', help='Serial port')
     parser.add_argument('-b', '--baud', default='115200', help='Baudrate')
     args = parser.parse_args()
@@ -175,25 +113,17 @@ if __name__ == "__main__":
     executor = futures.ThreadPoolExecutor(max_workers=10)
     robot_serial = executor.submit(SerialTasks, rio, cmdqueue, sensorqueue)
 
-    if args.ip_port is not None:
-        remote = Remote_IO(cmdqueue, sensorqueue)
-        server = grpc.server(executor)
-        sumo_pb2_grpc.add_SumoProtocolServicer_to_server(remote, server)
-        server.add_insecure_port('[::]:{0}'.format(args.ip_port))
-        server.start()
+    remote = Remote_IO(cmdqueue, sensorqueue)
+    server = grpc.server(executor)
+    sumo_pb2_grpc.add_SumoProtocolServicer_to_server(remote, server)
+    server.add_insecure_port('[::]:{0}'.format(args.ip_port))
+    server.start()
 
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            rio.disconnect()
-            server.stop(0)
-    else:
-        ctrl = KeyboardListener(cmdqueue)
-
-        try:
-            pyglet.app.run()
-        except KeyboardInterrupt:
-            rio.disconnect()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        rio.disconnect()
+        server.stop(0)
 
 
